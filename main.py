99894dcc34fde16c2ef92f148129dfa360c27a5a
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2021-05-20 00:41:52 +1000

    Docs + formatting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 """
-Modelling a python package
 
-Name, Author, Version, Description
 """
 
 from cmd import Cmd
@@ -23,8 +21,11 @@ gen_class: Generator.__class__ = None
 
 
 class CmdPrompt(Cmd):
-    prompt = NAME + '> '
-    intro = 'Welcome to the MODELER! Use ? command to list commands'
+    prompt = Formatting.format(NAME + '> ', Formatting.BOLD)
+    intro = Formatting.format("\nWelcome to the %s!\n" % NAME,
+                              [Formatting.GREEN, Formatting.BOLD]) + \
+            "Use ? command to list commands.\nBefore adding items, you must create a model using the 'new' command.\n" \
+            "You can use some example models by using the 'use' command."
 
     gen = None
 
@@ -32,10 +33,36 @@ class CmdPrompt(Cmd):
     store = None
 
     def do_exit(self, _user_input):
-        print("Exiting...")
+        """
+        Exits the program.
+
+        Arguments
+            None
+
+        Examples
+            > exit
+            Exiting...
+            0
+        """
+        print(Formatting.error("Exiting..."))
         return True
 
     def do_use(self, user_input):
+        """
+        Uses an example model.
+
+        Arguments
+            - Model number
+
+        Examples
+            > use 1
+            Setting 'PythonPackage' as the model...
+
+        Options
+            1    [ PythonPackage ] -- (Name, Author, Version, Description)
+            2    [      Fruit     ] -- (Name, Colour, Flavour)
+            3    [  CodeEditor   ] -- (Name, Developer, Primary Language, Price)
+        """
         model_object = fields = None
 
         if user_input == '1':
@@ -55,10 +82,28 @@ class CmdPrompt(Cmd):
         print(Formatting.format("Setting %r as the model..." % model_object, Formatting.GREEN))
 
     def do_new(self, model_object):
-        '''Creates a new model'''
+        """
+        Creates a new model.
+
+        Must have at least 3 fields. Enter the field name 'q' to exit input
+
+        Arguments
+            - Name (Optional, will be prompted)
+
+        Examples
+            > new
+            Model Name: Abc
+            Field 1 name: a
+            ...
+
+            > new Abc
+            Field 1 name: a
+            ...
+        """
+
         if model_object == "":
             # prompt for object
-            model_object = prompt("Type of object")
+            model_object = prompt("Model Name")
 
         model_object = format_model_name(model_object)
 
@@ -93,7 +138,24 @@ class CmdPrompt(Cmd):
         self.__set_model(model_object, fields)
 
     def do_add(self, _user_input):
-        '''Adds a new tuple to the store'''
+        """
+        Adds an item to the storage.
+
+        Must have a model set.
+
+        Arguments
+            None
+
+        Examples
+            > add
+            ---------[ New PythonPackage ]---------
+            Name: a
+            Author: b
+            Version: c
+            Description: d
+
+            ~~>  PythonPackage(Name='a', Author='b', Version='c', Description='d')
+        """
         if not self.__has_model():
             return
 
@@ -101,9 +163,40 @@ class CmdPrompt(Cmd):
         self.store.append(new_item)
 
     def do_list(self, _user_input):
+        """
+        Prints the items in the store.
+
+        Must have a model set and items in the store.
+
+        Arguments
+            None
+
+        Examples
+            > list
+            --------[ Printing Class Items ]--------
+            ~~>  PythonPackage(Name='a', Author='b', Version='c', Description='d')
+            -----------------------------------
+        """
         self.store.print()
 
     def do_search(self, user_input):
+        """
+        Searches for items with query.
+
+        Searches by checking if any attributes in the model starts with the query string.
+
+        Must have a model set and items in the store.
+
+        Arguments
+            - query
+
+        Examples
+            > search bob
+            Found 1 item...
+            --------[ Printing Class Items ]--------
+            ~~>  PythonPackage(Name='a', Author='bob', Version='c', Description='d')
+            -----------------------------------
+        """
         if not self.__has_model():
             return
 
@@ -131,7 +224,9 @@ class CmdPrompt(Cmd):
         self.gen = gen_class(object, fields)
         self.store = store_class(object=object)
 
-        self.prompt = "%s (%s)> " % (NAME, object)
+        self.prompt = Formatting.format("%s " % NAME, Formatting.BOLD) +\
+                      Formatting.format("(%s)" % object, [Formatting.BOLD, Formatting.PURPLE]) + \
+                      Formatting.format("> ", Formatting.BOLD)
 
 
 if __name__ == '__main__':
