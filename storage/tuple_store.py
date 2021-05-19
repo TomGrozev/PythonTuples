9956cf4ff01a821fb0b2e44cb7455e4bdbc13f9b
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2021-05-20 00:12:40 +1000

    Add tuple search update comment

--- storage/tuple_store.py
+++ storage/tuple_store.py
@@ -12,3 +12,9 @@ class TupleStore(Store):
 
     def __singular__(self, item):
         return self.object + item.__str__()
+
+    def __update_search__(self, item):
+        # Since tuples are not separate instances like classes, holding the searched number info is not feasible.
+        # This is possible but would require modifying the storage class to not be a list and rather be an object
+        # containing a list.
+        return item
